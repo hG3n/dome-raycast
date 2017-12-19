739d4c7bf657b57d80ec7e68a3710bee96542be0
Hagen Hiller <haghiller@gmail.com>
2017-12-19 15:19:26 +0100

    minor changes and tests

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 
 
-#include "Sphere.h"
+#include "Sphere.hpp"
+#include <glm/glm.hpp>
+#include <glm/geometric.hpp>
 
 int main() {
 
-    std::cout << "Fuckin' Raycast" << std::endl;
+    std::cout << "Raycast" << std::endl;
 
+    // create new sphere
     Sphere s = Sphere();
     std::cout << s << std::endl;
+
+    glm::vec3 a(1,2,3);
+    glm::vec3 b(2,3,4);
+
+    float r = glm::dot(a,b);
+
+    std::cout << r << std::endl;
 
     return 0;
 }
